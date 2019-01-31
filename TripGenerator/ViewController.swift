a002df2fb9c73e1a6e503dc24320945c381b1f24
Hemanth Yakkali <hy115@duke.edu>
2019-01-30 20:26:35 -0500

    Initial Commit

---
+++ TripGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripGenerator
+//
+//  Created by Hemanth Yakkali on 1/30/19.
+//  Copyright © 2019 Hemanth Yakkali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
