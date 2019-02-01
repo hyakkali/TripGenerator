2c632e589b198107b0c0bb7b4c5aabd14ce4cbfc
Hemanth Yakkali <hy115@duke.edu>
2019-02-01 01:18:55 -0500

    Cleaned up code and created functions to generate Expedia URLs

--- TripGenerator/Controller/MainViewController.swift
+++ TripGenerator/Controller/MainViewController.swift
@@ -14,17 +14,19 @@ class MainViewController: UIViewController, UIPickerViewDataSource, UIPickerView
     
     @IBOutlet weak var originTextField: UITextField!
     @IBOutlet weak var tripTypeTextField: UITextField!
-    @IBOutlet weak var departureTextField: UITextField!
-    @IBOutlet weak var arrivalTextField: UITextField!
     
-    var chosenCity = ""
-
     let originList = ["Raleigh", "Boston", "New York City"]
     let tripTypeList = ["Round Trip", "One Way"]
     let cityList = ["Paris", "London", "Bangkok", "Singapore", "New York", "Kuala Lumpur", "Hong Kong", "Dubai", "Istanbul", "Rome", "Shanghai", "Los Angeles", "Las Vegas", "Miami", "Toronto", "Barcelona", "Dublin", "Amsterdam", "Moscow", "Cairo", "Prague", "Vienna", "Madrid", "San Francisco", "Vancouver", "Budapest", "Rio de Janeiro", "Berlin", "Tokyo", "Mexico City", "Buenos Aires", "St. Petersburg", "Seoul", "Athens", "Jerusalem", "Seattle", "Delhi", "Sydney", "Mumbai", "Munich", "Venice", "Florence", "Beijing", "Cape Town", "Washington D.C.", "Montreal", "Atlanta", "Boston", "Philadelphia", "Chicago", "San Diego", "Stockholm", "Cancun", "Warsaw", "Sharm el-Sheikh", "Dallas", "Ho Chi Minh", "Milan", "Oslo", "Lisbon", "Punta Cana", "Johannesburg", "Antalya", "Mecca", "Macau", "Pattaya", "Guangzhou", "Kiev", "Shenzhen", "Bucharest", "Taipei", "Orlando", "Brussels", "Chennai", "Marrakesh", "Phuket", "Edirne", "Bali", "Copenhagen", "Sao Paulo", "Agra", "Varna", "Riyadh", "Jakarta", "Auckland", "Honolulu", "Edinburgh", "Wellington", "New Orleans", "Petra", "Melbourne", "Luxor", "Hanoi", "Manila", "Houston", "Phnom Penh", "Zurich", "Lima", "Santiago", "Bogota"]
     
     // URL to get valid airport code
     let AIRPORT_URL = "https://iatacodes.org/api/v6/autocomplete?api_key=9e145a85-8a4f-4f41-aaf3-e807721840ff&query="
+    var expedia_url = ""
+    var kayak_url = ""
+    var departDate = ""
+    var arrivalDate = ""
+    var origin = ""
+    var destination = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,30 +34,18 @@ class MainViewController: UIViewController, UIPickerViewDataSource, UIPickerView
         let originPickerView = UIPickerView()
         let tripTypePickerView = UIPickerView()
         
-        let departDatePicker = UIDatePicker()
-        departDatePicker.datePickerMode = UIDatePicker.Mode.date
-        departDatePicker.minimumDate = Date()
-        
-        let arrivalDatePicker = UIDatePicker()
-        arrivalDatePicker.datePickerMode = UIDatePicker.Mode.date
-        arrivalDatePicker.minimumDate = Date()
-        
         originPickerView.delegate = self
         tripTypePickerView.delegate = self
         
         originTextField.inputView = originPickerView
         tripTypeTextField.inputView = tripTypePickerView
-        departureTextField.inputView = departDatePicker
-        arrivalTextField.inputView = arrivalDatePicker
         
         originPickerView.tag = 1
         tripTypePickerView.tag = 2
         
         originTextField.text = originList[0]
         tripTypeTextField.text = tripTypeList[0]
-        
-        generateRandomDates()
-        
+                
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int { // number of columns
@@ -81,6 +71,7 @@ class MainViewController: UIViewController, UIPickerViewDataSource, UIPickerView
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) { // set text of textfield to row value
         if pickerView.tag == 1 {
             originTextField.text = originList[row]
+            origin = originList[row]
         } else {
             tripTypeTextField.text = tripTypeList[row]
         }
@@ -88,25 +79,29 @@ class MainViewController: UIViewController, UIPickerViewDataSource, UIPickerView
     
     @IBAction func generateTrip(_ sender: Any) {
         
-        // TODO: Create random dates
-        
         var number : Int = Int.random(in: 0 ..< cityList.count)
-        chosenCity = cityList[number]
+        destination = cityList[number]
         
-        while (chosenCity == originTextField.text!) {
+        while (destination == originTextField.text!) {
             number = Int.random(in: 0 ..< cityList.count)
-            chosenCity = cityList[number]
+            destination = cityList[number]
         }
         
-        Alamofire.request(AIRPORT_URL + chosenCity, method: .get).responseJSON { (response) in
+        Alamofire.request(AIRPORT_URL + destination, method: .get).responseJSON { (response) in
             if response.result.isSuccess {
                 print("Got airport data!")
                 let body : JSON = JSON(response.result.value!)
                 let airportName = body["response"]["airports_by_cities"][0]["code"].stringValue
-                print(self.chosenCity)
+                print(self.destination)
                 print(airportName)
                 
-                // TODO: Create Expedia and Kayak URLs
+                self.generateRandomDates()
+                
+                if (self.tripTypeTextField.text! == "Round Trip") {
+                    self.generateRTExpediaURL(origin: self.origin, dest: self.destination, departDate: self.departDate, arrivalDate: self.arrivalDate)
+                } else {
+                    self.generateOWExpediaURL(origin: self.origin, dest: self.destination, departDate: self.departDate)
+                }
                 
                 self.performSegue(withIdentifier: "goToTripPage", sender: self)
             } else {
@@ -119,11 +114,11 @@ class MainViewController: UIViewController, UIPickerViewDataSource, UIPickerView
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destinationVC = segue.destination as! TripViewController
         print("performing segue")
-        destinationVC.location = chosenCity
+        destinationVC.location = destination
     }
     
     func generateRandomDates() {
-//        var date = Date()
+
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MM dd y"
         let currentDate = dateFormatter.string(from: Date())
@@ -174,11 +169,34 @@ class MainViewController: UIViewController, UIPickerViewDataSource, UIPickerView
         
         print(arrMonth)
         print(arrDay)
-
+        
+        departDate = String(newMonth) + " " + String(newDay) + " " + String(currYear!)
+        arrivalDate = String(arrMonth) + " " + String(arrDay) + " " + String(arrYear)
+ 
     }
     
-    func generateExpediaURL() {
-        var URL = "https://www.expedia.com/Flights-Search?trip=roundtrip&leg1=from%3ABOS%2Cto%3ABRU%2Cdeparture%3A04%2F10%2F2019TANYT&leg2=from%3ABRU%2Cto%3ABOS%%2Cdeparture%3A04%2F16%2F2019TANYT&passengers=adults%3A1%2Cchildren%3A0%2Cseniors%3A0%2Cinfantinlap%3AY&options=cabinclass%3Aeconomy&mode=search&origref=www.expedia.com"
+    // MARK: - Generate URLS
+    
+    func generateRTExpediaURL(origin : String, dest : String, departDate : String, arrivalDate : String) {
+        
+        let departDateArr = departDate.split(separator: " ")
+        let arrivalDateArr = arrivalDate.split(separator: " ")
+        let departMonth = departDateArr[0]
+        let departDay = departDateArr[1]
+        let departYear = departDateArr[2]
+        let arrMonth = arrivalDateArr[0]
+        let arrDay = arrivalDateArr[1]
+        let arrYear = arrivalDateArr[2]
+        
+        expedia_url = "https://www.expedia.com/Flights-Search?trip=roundtrip&leg1=from%3A\(origin)%2Cto%3A\(dest)%2Cdeparture%3A\(departMonth)%2F\(departDay)%2F\(departYear)TANYT&leg2=from%3A\(dest)%2Cto%3A\(origin)%%2Cdeparture%3A\(arrMonth)%2F\(arrDay)%2F\(arrYear)TANYT&passengers=adults%3A1%2Cchildren%3A0%2Cseniors%3A0%2Cinfantinlap%3AY&options=cabinclass%3Aeconomy&mode=search&origref=www.expedia.com"
+    }
+    
+    func generateOWExpediaURL(origin : String, dest : String, departDate : String) {
+        let departDateArr = departDate.split(separator: " ")
+        let departMonth = departDateArr[0]
+        let departDay = departDateArr[1]
+        let departYear = departDateArr[2]
+        expedia_url = "https://www.expedia.com/Flights-Search?trip=oneway&leg1=from%3A\(origin)%2Cto%3A\(dest)%2Cdeparture%3A\(departMonth)%2F\(departDay)%2F\(departYear)TANYT&passengers=adults%3A1%2Cchildren%3A0%2Cseniors%3A0%2Cinfantinlap%3AY&options=cabinclass%3Aeconomy&mode=search&origref=www.expedia.com"
     }
 
 }
